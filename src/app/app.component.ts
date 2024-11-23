26bf2c95f53299cb423eb4dc5effbec7818e854b
vhtc <victor.tuy@hotmail.com>
2024-11-23 14:03:40 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabecalhoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
