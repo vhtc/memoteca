b36552b5aad2886fe542829fa98b41502d52bbde
vhtc <victor.tuy@hotmail.com>
2024-11-24 12:13:29 -0300

    att main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
+import { CriarPensamentoComponent } from "./componentes/pensamentos/criar-pensamento/criar-pensamento.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
