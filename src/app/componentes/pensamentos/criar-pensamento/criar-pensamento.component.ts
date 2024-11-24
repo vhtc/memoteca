0345e22e9a3daaba048300de30a27d649dea28e7
vhtc <victor.tuy@hotmail.com>
2024-11-24 12:13:04 -0300

    create criar-pensamento

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CabecalhoComponent } from "../../cabecalho/cabecalho.component";
+
+@Component({
+  selector: 'app-criar-pensamento',
+  imports: [CabecalhoComponent],
+  templateUrl: './criar-pensamento.component.html',
+  styleUrl: './criar-pensamento.component.css'
+})
+export class CriarPensamentoComponent {
+
+}
