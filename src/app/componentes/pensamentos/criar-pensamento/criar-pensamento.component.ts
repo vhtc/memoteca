e229682d25abe4cb6a7bfacfbbf29903aabb84dd
vhtc <victor.tuy@hotmail.com>
2024-11-28 08:59:11 -0300

    property binding and interpolação

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,10 +3,17 @@ import { CabecalhoComponent } from "../../cabecalho/cabecalho.component";
 
 @Component({
   selector: 'app-criar-pensamento',
-  imports: [CabecalhoComponent],
+  imports: [],
   templateUrl: './criar-pensamento.component.html',
   styleUrl: './criar-pensamento.component.css'
 })
 export class CriarPensamentoComponent {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
 
 }
