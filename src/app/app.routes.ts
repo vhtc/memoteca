6a9e1e96a7c972ac6eb7d8482a1801df3c0062d1
vhtc <victor.tuy@hotmail.com>
2024-11-28 12:01:42 -0300

    rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'listarPensamento'
+    },
+    {
+        path: 'criarPensamento',
+        component: CriarPensamentoComponent
+    },
+    {
+        path: 'listarPensamento',
+        component: ListarPensamentosComponent
+    }
+];
