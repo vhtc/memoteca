b1a4b1239fc94edbcdff22daee656b1461ea4e83
vhtc <victor.tuy@hotmail.com>
2024-11-28 12:01:18 -0300

    listar-pensamentos

---
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pensamentos',
+  imports: [],
+  templateUrl: './listar-pensamentos.component.html',
+  styleUrl: './listar-pensamentos.component.css'
+})
+export class ListarPensamentosComponent {
+
+}
